40ddc6590466c4ef86185f74dad8d187d1ff89b3
Bryan Moffit <moffit@jlab.org>
2022-12-02 03:52:47 -0500

    working version

--- gslTimerLib.c
+++ gslTimerLib.c
@@ -23,15 +23,17 @@ struct timespec tsStart, tsPrevious;
 uint32_t currentTimer = 0;
 uint32_t maxTimerUsed = 0;
 uint32_t gslTimerNHist = 0;
+int32_t gslTimerDebug = 0;
 
-#define rtERROR(format, ...) {fprintf(stderr,"%s: ERROR: ",__FUNCTION__); fprintf(stdout,format, ## __VA_ARGS__);}
+#define gtERROR(format, ...) {fprintf(stderr,"%s: ERROR: ",__FUNCTION__); fprintf(stderr,format, ## __VA_ARGS__);}
+#define gtDEBUG(format, ...) {if(gslTimerDebug==1) {fprintf(stdout,"%s: DEBUG:",__FUNCTION__); fprintf(stdout,format, ## __VA_ARGS__);}}
 
 int32_t
 gslTimerInit(uint32_t ntimers, uint32_t min_time, uint32_t max_time, uint32_t bin_size)
 {
   if(ntimers > MAX_GSL_TIMER)
     {
-      rtERROR("Invalid ntimers (%d).  Max = %d\n", ntimers, MAX_GSL_TIMER);
+      gtERROR("Invalid ntimers (%d).  Max = %d\n", ntimers, MAX_GSL_TIMER);
       return -1;
     }
 
@@ -91,32 +93,70 @@ int32_t
 gslTimerStartTime()
 {
   int32_t rval = 0;
+
+  /* Check if there's a previous measurement */
+  if(currentTimer > 0)
+    {
+      int32_t diff_total = tsPrevious.tv_nsec - tsStart.tv_nsec +
+	1000000000 * (tsPrevious.tv_sec - tsStart.tv_sec);
+
+      rval = gsl_histogram_increment (gslTimerTotalHist, diff_total);
+
+      gtDEBUG(" tot: rval = %d, diff_total = %d\n",
+	      rval, diff_total);
+
+    }
+
   rval = clock_gettime(CLOCK_MONOTONIC, &tsStart);
 
-  tsPrevious = tsStart;
+  tsPrevious.tv_sec = tsStart.tv_sec;
+  tsPrevious.tv_nsec = tsStart.tv_nsec;
   currentTimer = 0;
 
   return rval;
 }
 
 int32_t
+gslTimerEndTime()
+{
+  int32_t rval = 0;
+
+  gslTimerRecordTime();
+  /* Check if there's a previous measurement */
+  if(currentTimer > 0)
+    {
+      int32_t diff_total = tsPrevious.tv_nsec - tsStart.tv_nsec +
+	1000000000 * (tsPrevious.tv_sec - tsStart.tv_sec);
+
+      rval = gsl_histogram_increment (gslTimerTotalHist, diff_total);
+
+      gtDEBUG(" tot: rval = %d, diff_total = %d\n",
+	      rval, diff_total);
+
+      currentTimer = 0;
+    }
+
+  return 0;
+}
+
+int32_t
 gslTimerRecordTime()
 {
   int32_t rval = 0;
-  int diff_total, diff_previous;
+  int32_t diff_previous = 0;
 
   struct timespec tsCurrent;
   rval = clock_gettime(CLOCK_MONOTONIC, &tsCurrent);
 
-  diff_total = tsStart.tv_nsec - tsCurrent.tv_nsec +  1000000000 * (tsStart.tv_sec - tsCurrent.tv_sec);
-  diff_previous = tsPrevious.tv_nsec - tsCurrent.tv_nsec +  1000000000 * (tsPrevious.tv_sec - tsCurrent.tv_sec);
-
-  rval = gsl_histogram_increment (gslTimerTotalHist, diff_total);
-
   /* only fill the timer's we've allocated */
   if(currentTimer < gslTimerNHist)
     {
+      diff_previous = tsCurrent.tv_nsec - tsPrevious.tv_nsec +
+	1000000000 * (tsCurrent.tv_sec - tsPrevious.tv_sec);
+
       rval = gsl_histogram_increment (gslTimerHist[currentTimer], diff_previous);
+      gtDEBUG("%3d: rval = %d, diff_previous = %d\n",
+	      currentTimer, rval, diff_previous);
       maxTimerUsed = currentTimer;
       currentTimer++;
     }
@@ -134,18 +174,18 @@ gslTimerPrintStats()
   printf(" Allocated Histograms = %d   Used = %d\n",
 	 gslTimerNHist, maxTimerUsed);
 
-  for(ihist = 0; ihist < maxTimerUsed; ihist++)
+  for(ihist = 0; ihist <= maxTimerUsed; ihist++)
     {
-      printf("%2d: n = %4u   mean = %4.1f  sigma = %4.1f  \n",
-	     ihist,  (uint32_t)gslTimerHist[ihist]->n,
+      printf("%2d: n = %.1f   mean = %4.1f  sigma = %4.1f  \n",
+	     ihist,  gsl_histogram_sum(gslTimerHist[ihist]),
 	     gsl_histogram_mean(gslTimerHist[ihist]),
 	     gsl_histogram_sigma(gslTimerHist[ihist]));
     }
 
-  printf("TOTAL:\n      n = %4u   mean = %4.1f  sigma = %4.1f  \n",
-	 (uint32_t)gslTimerHist[ihist]->n,
-	 gsl_histogram_mean(gslTimerHist[ihist]),
-	 gsl_histogram_sigma(gslTimerHist[ihist]));
+  printf("TOTAL:\n    n = %.1f   mean = %4.1f  sigma = %4.1f  \n",
+	 gsl_histogram_sum(gslTimerTotalHist),
+	 gsl_histogram_mean(gslTimerTotalHist),
+	 gsl_histogram_sigma(gslTimerTotalHist));
 
   return 0;
 }
--- test/testTimers.c
+++ test/testTimers.c
@@ -3,7 +3,7 @@
  *    testTimers.c
  *
  * Description:
- *    Test Timers in the rolTimer library
+ *    Test Timers in the gslTimer library
  *
  *
  */
@@ -12,12 +12,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "rolTimerLib.h"
+#include <time.h>
+#include "gslTimerLib.h"
 
 int
 main(int argc, char *argv[])
 {
+  int i, n=10000;
 
+  gslTimerInit(10, 0, 500000, 100);
+
+  struct timespec tsWait = {0,150E3};
+
+  for(i = 0; i < n; i++)
+    {
+      gslTimerStartTime();
+      gslTimerRecordTime();//0
+      gslTimerRecordTime();//1
+      nanosleep(&tsWait, NULL);
+      gslTimerRecordTime();//2
+      gslTimerRecordTime();//3
+      gslTimerEndTime();//4
+    }
+
+  gslTimerPrintStats();
+  gslTimerFree();
 
   exit(0);
 }
