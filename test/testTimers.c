bc8feb42cba6abf46b5b1aca8bf5b1b31649ed01
Bryan Moffit <moffit@jlab.org>
2022-12-02 02:57:22 -0500

    initial commit of test program

---
+++ test/testTimers.c
@@ -0,0 +1,29 @@
+/*
+ * File:
+ *    testTimers.c
+ *
+ * Description:
+ *    Test Timers in the rolTimer library
+ *
+ *
+ */
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "rolTimerLib.h"
+
+int
+main(int argc, char *argv[])
+{
+
+
+  exit(0);
+}
+
+/*
+  Local Variables:
+  compile-command: "make -k testTimers "
+  End:
+*/
